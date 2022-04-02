faa73e524cedb452755e10b87988b761e22167da
David <DGawel77@gmail.com>
2022-04-02 17:21:50 -0500

    kml with correct file path

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function initMap() {
     });
 
     var ctaLayer = new google.maps.KmlLayer({
-        url: 'https://data.cityofchicago.org/api/geospatial/cauq-8yn6?method=export&format=KML',
+        url: './ChicagoBoundaries.kml',
         map: map
       });
     console.log("got run");
