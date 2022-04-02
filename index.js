7454aa3406e81fe07ec217ee777244d56f730d65
David <DGawel77@gmail.com>
2022-04-02 17:25:41 -0500

    cta kml

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 // Initialize and add the map
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.036 };
     // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
-    });
-
-    var ctaLayer = new google.maps.KmlLayer({
-        url: './ChicagoBoundaries.kml',
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 11,
+        center: {lat: 41.876, lng: -87.624}
+      });
+    
+      var ctaLayer = new google.maps.KmlLayer({
+        url: 'https://googlearchive.github.io/js-v2-samples/ggeoxml/cta.kml',
         map: map
       });
     console.log("got run");
