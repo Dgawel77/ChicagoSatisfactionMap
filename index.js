0d6260e5711f8fd2201a53d24adf40fd8c96bddd
David <DGawel77@gmail.com>
2022-04-02 18:00:51 -0500

    added color changing things

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 // Initialize and add the map
 function initMap() {
-    // The map, centered at Uluru
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 11,
         center: {lat: 41.876, lng: -87.624}
       });
     
     map.data.loadGeoJson("https://data.cityofchicago.org/api/geospatial/cauq-8yn6?method=export&format=GeoJSON");
-
+    
+    map.data.setStyle((feature) => {
+        let color = "gray";
+    
+        if (feature.getProperty("isColorful")) {
+          color = feature.getProperty("color");
+        }
+        return /** @type {!google.maps.Data.StyleOptions} */ {
+          fillColor: color,
+          strokeColor: color,
+          strokeWeight: 2,
+        };
+      });
+      
+      // When the user clicks, set 'isColorful', changing the color of the letters.
+      map.data.addListener("click", (event) => {
+        event.feature.setProperty("isColorful", true);
+      });
+      // When the user hovers, tempt them to click by outlining the letters.
+      // Call revertStyle() to remove all overrides. This will use the style rules
+      // defined in the function passed to setStyle()
+      map.data.addListener("mouseover", (event) => {
+        map.data.revertStyle();
+        map.data.overrideStyle(event.feature, { strokeWeight: 8 });
+      });
+      map.data.addListener("mouseout", (event) => {
+        map.data.revertStyle();
+      });
+    
     console.log("got run");
   }
   
